67ee5668b3a606f1e26081653eaa80de10204d35
Alex Raymondo <snackerson0@hotmail.com>
2018-08-18 20:18:09 -0400

    First Commit

---
+++ Realm Rush/Assets/EditorSnap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+
+    [SerializeField][Range(1,10)] private float gridSize = 10f;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Vector3 snapPosition;
+	    snapPosition.x =Mathf.RoundToInt(transform.position.x / gridSize)*gridSize ;
+	    snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+	    transform.position = new Vector3(snapPosition.x,0f,snapPosition.z);
+	}
+}
